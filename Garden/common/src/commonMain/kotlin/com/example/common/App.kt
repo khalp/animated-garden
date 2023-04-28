32277f30a9d0fa14b4504f1816e5d64b12c7db36
Kristen Halper <khalper@microsoft.com>
2023-04-28 16:04:14 -0700

    Draw a sunflower!

--- Garden/common/src/commonMain/kotlin/com/example/common/App.kt
+++ Garden/common/src/commonMain/kotlin/com/example/common/App.kt
@@ -1,7 +1,20 @@
 package com.example.common
 
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.lazy.grid.GridCells
+import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+
 @Composable
 fun MyGarden() {
-    
+    LazyVerticalGrid(
+        modifier = Modifier.fillMaxSize(),
+        columns = GridCells.Adaptive(100.dp)
+    ) {
+        items(8) {
+            Sunflower()
+        }
+    }
 }
---
+++ Garden/common/src/commonMain/kotlin/com/example/common/Sunflower.kt
@@ -0,0 +1,51 @@
+package com.example.common
+
+import androidx.compose.foundation.Canvas
+import androidx.compose.foundation.layout.size
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.geometry.Offset
+import androidx.compose.ui.geometry.Size
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.drawscope.DrawScope
+import androidx.compose.ui.graphics.drawscope.rotate
+import androidx.compose.ui.unit.dp
+
+private val Brown = Color(0xFF742C0D)
+private val Yellow = Color(0xFFF8D314)
+
+@Composable
+fun Sunflower() {
+    Canvas(Modifier.size(100.dp)) {
+        drawSunflower()
+    }
+}
+
+fun DrawScope.drawSunflower() {
+    drawPetals()
+    drawCenter()
+}
+
+fun DrawScope.drawCenter() {
+    val radius = 20f
+
+    drawCircle(color = Brown, radius = radius, center = center)
+}
+
+fun DrawScope.drawPetals() {
+    val numPetals = 8
+    var angle = 0f
+    val size = Size(width = 30f, height = 75f)
+
+    repeat(numPetals) {
+        rotate(angle) {
+            drawOval(
+                color = Yellow,
+                topLeft = Offset(center.x - size.width / 2, center.y),
+                size = size
+            )
+        }
+        angle += 360 / numPetals
+    }
+}
+
